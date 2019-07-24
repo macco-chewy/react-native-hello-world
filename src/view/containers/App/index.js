544217f4bd344f31347f701dc1cf01875c2f3c76
Mike Cherry <mcherry@zamphear.net>
2019-07-23 21:32:31 -0700

    Added react-native-config capabilities

--- src/view/containers/App/index.js
+++ src/view/containers/App/index.js
@@ -1,8 +1,12 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
+import Config from 'react-native-config';
+
 import Home from 'screens/Home';
 import Fetch from 'screens/Fetch';
 import Test from 'screens/Test';
+
+console.log(Config.TEST_VAR_1);
 
 const Navigator = createStackNavigator({
   Home: { screen: Home },
